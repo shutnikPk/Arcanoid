a3c1cdde68574927730f70eadd2e95716703f3c4
ShutnikPk <shutnikssspk@gmail.com>
2020-08-12 12:18:54 +0300

    add class for game

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-window.addEventListener(load, ()=>{
-    
+window.addEventListener("DOMContentLoaded", ()=>{
+    let containerTag = document.createElement("div");
+    let canvas = document.createElement("CANVAS");
+    canvas.width = 800;
+    canvas.heigth = 600;
+    let ctx = canvas.getContext('2d');
+    document.body.prepend(containerTag);
+    document.body.firstChild.appendChild(canvas);
+
+    class Block {
+        constructor (isDestroyed, isBonus){
+            this.isDestroyed = isDestroyed;
+            this.isBonus = isBonus;
+        }
+    }
+
+    class BorderBlock extends Block{
+        
+    }
 })
