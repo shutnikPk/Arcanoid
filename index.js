2f9a5243d442d989c5cb979e73ff1e43d90c6419
shutnikPk <shutnikssspk@gmail.com>
2020-08-14 21:36:28 +0300

    change drawBlocks method

--- index.js
+++ index.js
@@ -74,26 +74,25 @@ window.addEventListener("DOMContentLoaded", ()=>{
             [{x:2, y:0}, {x:2, y:1}, {x:2, y:2}, {x:2, y:3}, {x:2, y:4}, {x:2, y:5}, {x:2, y:6}, {x:2, y:7}],
             [{x:3, y:0}, {x:3, y:1}, {x:3, y:2}, {x:3, y:3}, {x:3, y:4}, {x:3, y:5}, {x:3, y:6}, {x:3, y:7}],
             [{x:5, y:0}, {x:5, y:1}, {x:5, y:2}, {x:5, y:3}, {x:5, y:4}, {x:5, y:5}, {x:5, y:6}, {x:5, y:7}]
-        ];        
+        ];   
 
-        drawBlocks () {
-            let destroyedBlock = [];
+            
+
+        createBlocks () {
+           let destroyedBlock = [];
             for (let row = 0; row < this.field.length; row++){
                 destroyedBlock[row] = [];
                 for (let cell = 0; cell < this.field[row].length; cell++){  
                     destroyedBlock[row][cell] = new gameObj(25 + cell * 93.75, 24.5 + row * 30, 93.6, 30, true);
-                    destroyedBlock[row][cell].drawObj('orange');
+                    //destroyedBlock[row][cell].drawObj('orange');
                 }
             } 
-
+            return destroyedBlock;
         }
-
         clearZone (x, y, width, heigth) {
             ctx.clearRect(x, y, width, heigth);
         }
-
         interval = setInterval(updateGameArea, 20);
-
     }    
 
     let topBorder = new gameObj(0, 0, 800, 25, false);
@@ -109,7 +108,7 @@ window.addEventListener("DOMContentLoaded", ()=>{
     let player = new gameObj (325, 560, 100, 15, false);  
     let ball = new gameObj (350, 533, 25, 25, false);
     let gameField = new Map(0);
-    gameField.drawBlocks();        
+    gameField.createBlocks();        
 
     ball.speedY = 2;
     ball.speedX = 2;
@@ -124,14 +123,20 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
     function updateGameArea() {        
         gameField.clearZone(25, 560, 750, 15);    
-        gameField.clearZone(25, 25, 750, 550); 
+        gameField.clearZone(24, 24, 750, 550); 
+        for (let row = 0; row < gameField.field.length; row++){
+            //gameField.destroyedBlock[row] = [];
+            for (let cell = 0; cell < gameField.field[row].length; cell++){                  
+                gameField.createBlocks()[row][cell].drawObj('orange');
+            }
+        }
         player.speedX = 0;
         
         console.log('playerCenter is '+player.newCenterX ());
         console.log(ball.x);
-        if(ball.onClosion(leftBorder)||ball.onClosion(rigthBorder)){ ball.speedX *=-1; console.log('sideBorder');}
-        if(ball.onClosion(topBorder)){ ball.speedY *=-1; console.log('topBorder');} 
-        if(ball.onClosion(player)&&ball.x<player.newCenterX()){ ball.speedY *=-1;ball.speedX *=-1; console.log('player');}
+        if(ball.onClosion(leftBorder)||ball.onClosion(rigthBorder)){ ball.speedX *=-1; console.log('sideBorder')}
+        if(ball.onClosion(topBorder)){ ball.speedY *=-1; console.log('topBorder')} 
+        if(ball.onClosion(player)&&ball.x<player.newCenterX()){ ball.speedY *=-1;ball.speedX *=-1; console.log('player')}
         if(ball.onClosion(player)&&ball.x>player.newCenterX()){ ball.speedY *=-1; console.log('player');}   
         if(ball.onClosion(bottomBorder)){ ball.speedY *=-1;clearInterval(gameField.interval);console.log('gameOwer')}        
         if (gameField.key && gameField.key == 37) {player.speedX = -10;}
